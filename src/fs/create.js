a697bbdeb3277f0cb139ddd29ad2a57b6f8e705d
Nikolai Syrovezhko <N.Syrovezhko@transtelematica.ru>
2024-01-25 12:31:55 +0300

    feat: `fs:create` solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import path from "path";
+import { writeFile } from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/fs/files/fresh.txt");
+  new Promise((resolve, reject) => {
+    writeFile(target, "I am fresh and young", {
+      flag: "wx",
+    })
+    .catch(() => {
+      throw new Error("FS operation failed")
+    });
+  });
 };
 
 await create();
