3450ccb584bde557024d76d7c61b8c782722ddab
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 20:23:36 +0300

    feat: `fs:read` solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import path from "path";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/fs/files/fileToRead.txt");
+  fs.readFile(target, (err, data) => {
+    if (data) console.log(data.toString("utf8"));
+    else throw new Error("FS operation failed");
+  });
 };
 
 await read();
