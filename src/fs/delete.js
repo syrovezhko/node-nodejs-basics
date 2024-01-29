4ed057d0fd10e264207ce81034e5d7747b67c8dd
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 19:48:40 +0300

    feat: `fs:delete` solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import path from "path";
+import { unlink } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/fs/files/fileToRemove.txt");
+  try {
+    await unlink(target);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
