dca41888d89771aa4bc9941c4d756b19756a0670
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 20:12:52 +0300

    feat: `fs:list` solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import path from "path";
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/fs/files");
+  await fs.readdir(target, (err, files) => {
+    if (files) console.log(files);
+    else throw new Error("FS operation failed");
+  });
 };
 
 await list();
