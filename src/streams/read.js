df7ab0f2db2f9a786914591516c4766a9438354c
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 22:00:15 +0300

    feat: `streams:read` solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from "path";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/streams/files/fileToRead.txt");
+  const stream = fs.createReadStream(target);
+
+  stream.on("data", (data) => {
+    process.stdout.write(data.toString("utf8").concat("\n"));
+  });
 };
 
 await read();
