68da334556b9b40f90ba056a5e1a26277a377954
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 21:52:01 +0300

    feat: `hash` solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import path from "path";
+import crypto from "crypto";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const target = path.resolve("./src/hash/files/fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256");
+  const stream = fs.createReadStream(target);
+
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  stream.on("end", () => {
+    const result = hash.digest("hex");
+    console.log(result);
+  });
 };
 
 await calculateHash();
