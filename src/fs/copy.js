6c5413a0b210b1b7f74bf3dbab6d417fd61453c3
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-29 19:47:26 +0300

    refactor: `fs:copy.js` solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,19 +3,19 @@ import fs from "fs";
 
 const copy = async () => {
   const root = path.resolve("./src/fs");
-  const src = root.concat("/files")
-  const dest = root.concat("/files_copy")
+  const src = root.concat("/files");
+  const dest = root.concat("/files_copy");
   try {
     await fs.stat(dest, (exist) => {
-      if(!exist) throw new Error("FS operation failed")
-    })
-    await fs.cp(src, dest, {recursive: true, overwrite: false}, (err) => {
-      if(err){
-        throw new Error("FS operation failed")
+      if (!exist) throw new Error("FS operation failed");
+    });
+    await fs.cp(src, dest, { recursive: true, overwrite: false }, (err) => {
+      if (err) {
+        throw new Error("FS operation failed");
       }
     });
   } catch (e) {
-    throw new Error("FS operation failed")
+    throw new Error("FS operation failed");
   }
 };
 
