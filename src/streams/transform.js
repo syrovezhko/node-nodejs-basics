50a0d187254e54afbf149b6c03f0a3e80f0da69f
Nikolai Syrovezhko <N.Syrovezhko@transtelematica.ru>
2024-01-30 09:43:45 +0300

    feat: `streams:transform` solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  let stringNum = 1;
+  process.stdout.write("Input text and press ENTER.\n\n");
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split("").reverse().join("");
+      callback(
+        null,
+        `\n\nThis is the ${stringNum++}-th line${"_".repeat(
+          reversedChunk.length < 20 ? 0 : reversedChunk.length - 22,
+        )}`.concat(
+          reversedChunk.concat(
+            `\n${"‾".repeat(
+              reversedChunk.length < 21 ? 21 : reversedChunk.length - 1,
+            )}\n\n`,
+          ),
+        ),
+      );
+    },
+  });
+
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
