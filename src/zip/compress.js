6cbad8d89b3ea67bf42aeaa2fbe5a6ba5bab9760
Nikolai Syrovezhko <N.Syrovezhko@transtelematica.ru>
2024-01-30 10:24:48 +0300

    feat: `zip:compress` solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import path from "path";
+import { createGzip } from "zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  const dir = path.resolve("./src/zip/files");
+  const gzip = createGzip();
+  const src = fs.createReadStream(dir.concat("/fileToCompress.txt"));
+  const result = fs.createWriteStream(dir.concat("/archive.gz"));
+
+  src.pipe(gzip).pipe(result);
 };
 
 await compress();
