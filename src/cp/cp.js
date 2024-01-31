266c58990f093a6f5bc981251461501d2754cdf8
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-01-31 19:45:03 +0300

    feat: `cp` solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import path from "path";
+import { fork } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const target = path.resolve("./src/cp/files/script.js");
+  const cp = fork(target, args, { silent: false });
+
+  cp.on("message", (data) => {
+    process.stdout.write(`message from child process: ${data}`);
+  });
+
+  cp.on("close", (data) => {
+    process.stdout.write(`child process closed with code: ${data}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Let's", "do", "it!"]);
